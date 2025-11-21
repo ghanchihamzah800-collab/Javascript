7053eafcd13f996c1889b359710833add18b4acf
Ghanchi Hamzah <ghanchihamzah800@gmail.com>
2025-11-21 12:40:17 +0000

    Done Operations and data type convertion

---
+++ 01_basics/operations.js
@@ -0,0 +1,12 @@
+
+//Assigning variables and values:
+let value= 3;
+let negValue = -value; //Changed positive value to negative.
+
+//Printing out the value & it's type:
+console.log(negValue);
+console.log (typeof negValue);
+
+//Changing type of value from Number to Boolean:
+let valueBoolean= Boolean(value);
+console.log (typeof valueBoolean);
