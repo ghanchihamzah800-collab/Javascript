4a817f39fb8d6306d4ec8c6d6ce53abd7f677b0c
Ghanchi Hamzah <ghanchihamzah800@gmail.com>
2025-12-27 19:06:54 +0000

    Some Comaprision Concepts

---
+++ 03_Practice/4_ComparisionAndEquality.js
@@ -0,0 +1,39 @@
+/* 1.	Compare using both: == and ===
+"5" and 5, true and 1, null and undefined */
+
+console.log("5" == 5);
+console.log("5" === 5);
+console.log("\n")
+
+console.log(true == 1);
+console.log(true === 1);
+console.log("\n")
+
+console.log(null == undefined)
+console.log(null === undefined)
+console.log("\n")
+
+//-----------------
+
+//2.	Compare "Apple" and "Banana".
+
+console.log("Apple" == "Banana")
+console.log("Apple" === "Banana")
+console.log("\n")
+
+//----------------
+
+//3.	Compare "2" and 12.
+
+console.log("2" == 12)
+console.log("2" === 12)
+console.log("\n")
+
+//------------------
+
+//4.	Compare arrays: [] == [], [] === [].
+
+console.log([] == [])
+
+
+//-----------------
