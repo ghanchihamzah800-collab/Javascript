9ae493298c580b941b850ff9d65e6893ecb39294
Ghanchi Hamzah <ghanchihamzah800@gmail.com>
2025-12-27 14:09:29 +0000

    Type Convesion of Data types

---
+++ 03_Practice/3_typeConversion.js
@@ -0,0 +1,32 @@
+// 1.	Convert "45" to number.
+
+let num = "45"
+console.log(Number(num));
+console.log(typeof(Number(num)));
+
+// 2.	Convert false to number.
+
+let bool = false
+console.log(Number(bool));
+console.log(typeof(Number(bool)));
+
+// 3.	Convert number 55 to string.
+
+let num1 = 55
+console.log(String(num1));
+console.log(typeof (String(num1)));
+
+// 4.	Convert "33abc" to number and observe.
+
+let ab = "33abc"
+console.log(Number(ab));
+
+// 5.	Convert "", " ", "0" to boolean.
+
+let str1 = ""
+let str2 = " "
+let str3 = "0"
+
+console.log(Boolean (str1));
+console.log(Boolean (str2));
+console.log(Boolean (str3));
