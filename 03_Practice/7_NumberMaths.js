73166aa1005472bbf348b38da8a3fd591be86e7f
Ghanchi Hamzah <ghanchihamzah800@gmail.com>
2025-12-28 12:22:31 +0000

    Math methods

---
+++ 03_Practice/7_NumberMaths.js
@@ -0,0 +1,39 @@
+//1.	Round 4.75 using Math.round, Math.ceil, and Math.floor.
+
+a= 4.75
+
+console.log(Math.round(a))
+console.log(Math.ceil(a))
+console.log(Math.floor(a))
+console.log("\n")
+
+//-----------
+
+//2.	Generate a random number from 1-100.
+
+console.log(Math.floor(Math.random() *100) + 1)
+console.log("\n")
+
+//------------
+
+//3.	Convert 45.98765 to 2 decimals.
+
+let num = 45.98765;
+console.log(num.toFixed(2));
+console.log("\n")
+
+//------------------
+
+//4.	Check if a value is NaN.
+
+let value = "abc";
+console.log(Number.isNaN(value));
+console.log("\n")
+
+//---------------
+
+//5.	Get absolute value of -99.
+
+console.log(Math.abs(-99));
+console.log("\n")
+
